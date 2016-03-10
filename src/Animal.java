dac4ae5a86ae2995ec198778b167646b179f073b
chintan <chintan791@gmail.com>
2016-03-10 13:02:04 -0500

    commit message

---
+++ src/Animal.java
@@ -0,0 +1,25 @@
+
+public class Animal {
+
+
+
+	
+	public Animal()
+	{
+		System.out.println("A animal is here");
+	}
+	
+	public void sleep(){
+		System.out.println("A animal sleeps");
+	}
+	
+	public void eat(){
+		System.out.println("A animal eats");
+	}
+	
+
+}
+
+
+
+
---
+++ src/Cat.java
@@ -0,0 +1,24 @@
+
+public class Cat extends Animal{
+
+	
+	public Cat()
+	{
+		super();
+		System.out.println("A cat is here");
+	}
+	
+	public void sleep(){
+		System.out.println("A cat sleeps");
+	}
+	
+	public void eat(){
+		System.out.println("A cat eats");
+	}
+	
+	public void pur(){
+		System.out.println("A cat purs");
+	}
+
+	
+}
